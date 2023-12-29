42732d257f7eeb5292445cd332314f33484bf40f
mxvements <luciama2@student.42madrid.com>
2023-12-29 17:26:41 +0100

    automata for parsing

--- srcs/parse_automata.c
+++ srcs/parse_automata.c
@@ -23,10 +23,10 @@ t_dll	**evalerror(t_dll **ptlst)
 
 t_dll	**evalpoint(char *s, t_dll **ptlst)
 {
-	const int	comma = ft_strchr(s, ',');
+	const int	comma = ft_strchri(s, ',');
 	int			atoiflag;
 	t_ptcont	*ptinfo;
-	t_dll		*pt;
+	t_dll		*ptnode;
 
 	if (!s)
 		return (NULL); //need to protect the previous substr
@@ -37,28 +37,29 @@ t_dll	**evalpoint(char *s, t_dll **ptlst)
 	ptinfo->height = ft_atoif(s, &atoiflag);
 	if (atoiflag < 0)
 		return (evalerror(ptlst));
-	/*if (comma != 0)
-		//do color, starts in (s + comma)
+	if (comma != 0)
+		ptinfo->color = 0x33FFBD; //TODO: change to the actual info
 	else
-		//ptinfo->color == WHITE */
-	pt = ft_dllnew((void *)ptinfo);
-	if (!pt)
+		ptinfo->color = 0x00;
+	ptnode = ft_dllnew((void *)ptinfo);
+	if (!ptnode)
 		return (evalerror(ptlst));
-	ft_dlladd_back(ptlst, pt);	
+	ft_dlladd_back(ptlst, ptnode);
+	free(s);
 	return (ptlst);
 }
 
 size_t	a_getstate(int i, int j)
 {
-	const size_t	t_states[][5] = {
+	const size_t	t_states[][5] = {\
 	// (j)
-	//	0  1  2  3  4
-	//	/s, -+, D, \n, ^	// (i)
+	//   0  1  2  3  4
+	//	/s,-+, D, \n, ^	// (i)
 		{4, 2, 3, 4, 1},	//	0	INIT
 		{1, 1, 1, 1, 1},	//	1	ERR
 		{4, 1, 3, 4, 1},	//	2	SIGN '-' '+'
 		{4, 1, 3, 4, 1},	//	3	DICT "0123456789abcdefx,"
-		{4, 2, 3, 4, 1},	//	4	SPACE or \n
+		{4, 2, 3, 4, 1}		//	4	SPACE or \n
 	};
 	return (t_states[i][j]);
 }
@@ -95,13 +96,13 @@ t_dll	**a_parse(char *s, t_dll **ptlst)
 		ostate = a_changestate(s[i], state);
 		if ((state == 0 || state == 4) && (ostate == 2 || ostate == 3))
 			init_pt = i;
-		if (state == 3 && (ostate == 4 || s[i] == '\0'))
+		if ((state == 3 && ostate == 4) || (ostate == 3 && s[i + 1] == '\0'))
 		{
 			ptlst = evalpoint(ft_substr(s, init_pt, (i - init_pt) + 1), ptlst);
 			if (!ptlst)
 				return (NULL);
 		}
-		if (ostate == 1 || (ostate < 3 && s[i] == '\0'))
+		if (ostate == 1 || (ostate < 3 && s[i + 1] == '\0'))
 			evalerror(ptlst);
 		state = ostate;
 		i++;

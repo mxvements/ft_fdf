0b3a4ce3399b4494329ee4099beeb0b22517ca2f
mxvements <luciama2@student.42madrid.com>
2024-01-24 20:36:19 +0100

    checked filetype for maps

--- srcs/main.c
+++ srcs/main.c
@@ -16,17 +16,33 @@
 {
 	system("leaks -q fdf");
 }*/
+
+static int	checkfile(char *path)
+{
+	const size_t	len = ft_strlen(path);
+	const char		*filetype = (path + (len - 4));
+
+	return (ft_strncmp(filetype, ".fdf", 4));
+}
+
 //atexit(leaks);
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
+		if (checkfile(argv[1]) != 0)
+		{
+			write(1, ANSICOLOR_RED, 6);
+			ft_putstr_fd("Error. Wrong file ending.\n", 2);
+			write(1, ANSICOLOR_RED, 5);
+			return (-1);
+		}
 		fdf(argv[1]);
 	}
 	else if (argc > 2)
 	{
 		write(1, ANSICOLOR_RED, 6);
-		ft_putstr_fd("Error.\n", 2);
+		ft_putstr_fd("Error. Arguments: \"./fdf ./file.fdf\"\n", 2);
 		write(1, ANSICOLOR_RESET, 5);
 	}
 	return (0);
